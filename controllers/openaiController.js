c976c21cec97488d4d2ff8f67c163470ab1497f9
pooria eshghi malayeri <100807060+PooriaEshghi@users.noreply.github.com>
2022-12-27 12:21:05 +0100

    fix request image

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -6,34 +6,37 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
-    const {prompt, size} = req.body;
+  const { prompt, size } = req.body;
 
-    const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
-    try {
-        const response = await openai.createImage({
+  const imageSize =
+    size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
 
-            prompt,
-            n:1,
-            size: imageSize
-        });
-         const imageUrl = response.data.data[0].url
-         res.status(200).json({
-            success:true,
-            data:imageUrl
-         });
-        } catch (error) {
-            if (error.response) {
-                console.log(error.response.status);
-                console.log(error.response.data);
-              } else {
-                console.log(error.message);
-              }
-            res.status(400).json({
-                success:false,
-                error: 'The image not be generated'
-            });
-        
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: imageSize,
+    });
+
+    const imageUrl = response.data.data[0].url;
+
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
     }
+
+    res.status(400).json({
+      success: false,
+      error: 'The image could not be generated',
+    });
+  }
 };
 
-module.exports = {generateImage}
+module.exports = { generateImage };
